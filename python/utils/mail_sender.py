5f5966ccf84a84a9c328b167814733d186f13f2a
Matvey Bagrov <matvey.bagr77@mail.ru>
2023-12-23 00:04:16 +0300

    Фикс админской рассылки + фикс отправки почты

--- python/utils/mail_sender.py
+++ python/utils/mail_sender.py
@@ -2,32 +2,23 @@ import os
 import smtplib
 from email.mime.text import MIMEText
 
-from dotenv import load_dotenv
+# Почта Mail.ru и пароль
+mailru_user = "polynavibot@mail.ru"  # Замените на ваш адрес на Mail.ru
+mailru_password = os.getenv('MAIL_PASSWORD')  # Установите переменную окружения MAILRU_PASSWORD
 
-# ЗАПОЛНИТЬ (нужна именно gmail почта и еще включенная настройка) В настройках почты ->
-# Ненадежные приложения, у которых есть доступ к аккаунту -> ON
-# Почта с которой идет письмо
-gmail_user = "matvik2002@gmail.com"
-gmail_password = os.getenv('GMAIL_PASSWORD')
-
-# Почта ЦОКа
-to = "matvey.bagr77@mail.ru"  # ЗАПОЛНИТЬ "ceq@spbstu.ru"
-load_dotenv()
+# Почта получателя
+to = "pal4ik228@mail.ru"
 
 def send_mail(question, mail, fio):
-    message = question + f"\n\nПожалуйста, пришлите ответ на почту: {mail}\n\n\nПисьмо сформировано при помощи " \
-                         f"телеграм-бота для помощи студентам."
-    subject = "Вопрос в ЦКО от " + fio
-
-    server = smtplib.SMTP("smtp.gmail.com", 587)
-    server.starttls()
-
+    msg = MIMEText(question + f"\n\nПожалуйста, пришлите ответ на почту: {mail}\n\n\nПисьмо сформировано при помощи " \
+                         f"телеграм-бота для помощи студентам.")
+    msg['Subject'] = "Вопрос в ЦКО от " + fio
+    msg['From'] = mailru_user
+    msg['To'] = to
     try:
-        server.login(gmail_user, gmail_password)
-        msg = MIMEText(message)
-        msg["Subject"] = subject
-        server.sendmail(gmail_user, to, f"{msg.as_string()}")
-
+        with smtplib.SMTP_SSL('smtp.mail.ru', 465) as server:
+            server.login(mailru_user, os.getenv('GMAIL_PASSWORD'))
+            server.send_message(msg)
         return "The message was sent!"
     except Exception as _ex:
         return f"{_ex}\n Something went wrong!"
